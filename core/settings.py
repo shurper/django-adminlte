98bb4ae56bcab8c6ddd8964d908d3e36e4fe3de1
Alex <da.tonacom@gmail.com>
2024-07-08 21:27:49 +0300

    fix

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,7 @@ DEBUG = str2bool(os.environ.get('DEBUG'))
 ALLOWED_HOSTS = ['*']
 
 # Add here your deployment HOSTS
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:5085', 'http://127.0.0.1:8000', 'http://127.0.0.1:5085']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:5085', 'http://127.0.0.1:8000', 'http://127.0.0.1:5085', 'http://metrics.tonantis.com', 'https://metrics.tonantis.com']
 
 #Render Context
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
@@ -101,7 +101,7 @@ CORS_ALLOW_HEADERS = [
     'x-requested-with',
 ]
 
-ALLOWED_HOSTS = ['wildberries.ru','127.0.0.1','tonantis.com']
+ALLOWED_HOSTS = ['wildberries.ru','127.0.0.1','metrics.tonantis.com']
 
 # Специальный URL
 #CORS_URLS_REGEX = r'^/wildberries/api/observer/report$'
