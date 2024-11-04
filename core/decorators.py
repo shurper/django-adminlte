3fe8e502583fc52450e79f3a35267ff3a2c113a0
Alex <da.tonacom@gmail.com>
2024-11-04 20:20:29 +0300

    fix

--- core/decorators.py
+++ core/decorators.py
@@ -5,7 +5,10 @@ def localhost_only(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
         allowed_ips = ['127.0.0.1', '::1']  # IPv4 и IPv6 для localhost
-        if request.META['REMOTE_ADDR'] not in allowed_ips:
-            return HttpResponseForbidden("Access Denied")
+        client_ip = request.META.get('REMOTE_ADDR')
+
+        if client_ip not in allowed_ips:
+            return HttpResponseForbidden(f"Access Denied for {client_ip}")
+
         return view_func(request, *args, **kwargs)
     return _wrapped_view
