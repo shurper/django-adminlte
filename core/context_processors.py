cfa69eb07f55708a150b9d2efe181ef4cf697701
Alex <da.tonacom@gmail.com>
2025-05-22 12:49:46 +0300

    upgrade

--- core/context_processors.py
+++ core/context_processors.py
@@ -2,3 +2,9 @@ from notification.services.notifiers import unread_notifications
 
 def notifications_context(request):
     return unread_notifications(request)
+
+
+def segment_processor(request):
+    return {
+        'segment': request.resolver_match.url_name if request.resolver_match else ''
+    }
