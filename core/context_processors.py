00f45a41199d1760bf707e4f9a07308584a1e887
Alex <da.tonacom@gmail.com>
2025-05-23 13:54:42 +0300

    upgrade

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,6 +1,11 @@
 from notification.services.notifiers import unread_notifications
 
 def notifications_context(request):
+    if not request.user.is_authenticated:
+        return {
+            'unread_notifications': [],
+            'unread_count': 0
+        }
     return unread_notifications(request)
 
 
--- notification/services/notifiers.py
+++ notification/services/notifiers.py
@@ -5,6 +5,12 @@ from core.utils.cache import cached_user_function
 
 @cached_user_function(timeout=60, key_prefix='unread_notifications')
 def unread_notifications(request):
+    if not request.user.is_authenticated:
+        return {
+            'unread_notifications': {},
+            'unread_count': 0
+        }
+
     qs = Notification.objects.unread().filter(recipient=request.user)
     unread = list(qs.order_by('-timestamp')[:5])
     unread_count = qs.count()
